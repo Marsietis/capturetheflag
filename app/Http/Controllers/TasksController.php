f2949ed6cfe518a676ffa07ee7edf823ea1bbf64
Marsietis <martynas.matijosius@gmail.com>
2023-06-22 18:46:57 +0300

    Score system working

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
-use http\Client\Curl\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\DB;
