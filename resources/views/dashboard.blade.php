abf1ce8b2c43d9ba094b817691e45a109336f7f6
Marsietis <martynas.matijosius@gmail.com>
2023-06-22 21:17:26 +0300

    Displays success on redirect

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,10 @@
 <x-app-layout>
+
+    @if(session()->has('success'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">{{ session()->get('success') }}</strong>
+        </div>
+    @endif
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
