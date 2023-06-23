162ede021b1aae062d4b8f693778501e807ee461
Marsietis <martynas.matijosius@gmail.com>
2023-06-23 19:45:38 +0300

    leaderboard.blade.php styling

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -5,24 +5,35 @@
         </h2>
     </x-slot>
 
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center mb-6">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center pt-12">
         <div class="bg-white dark:bg-zinc-900 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900 dark:text-gray-100 text-xl">
-                Your score: {{ $user->score }} Your position: {{ $userPosition }}/{{ $users->count() }}
+            <div class="p-6 text-gray-900 dark:text-gray-100 text-xl text-center">
+                Your score: {{ $user->score }} Your position: {{ $userPosition }} / {{ $users->count() }}
+                @if($userPosition === 1)
+                    <div class="text-4xl">🏆 You are the leader 🏆</div>
+                @endif
             </div>
         </div>
     </div>
 
-    <div class="text-5xl text-white m-20">
-        @foreach($users as $index => $user)
-            @if($user->id === Auth::user()->id)
-                @php
-                    $userPosition = $index + 1;
-                @endphp
-                <span class="bg-yellow-500">{{ $userPosition }}. {{ $user->name }} ({{ $user->score }})</span><br>
-            @else
-                {{ $index+1 }}. {{ $user->name }} ({{ $user->score }})<br>
-            @endif
-        @endforeach
+    <div class="flex justify-center text-white text-7xl mt-3">Top players</div>
+    <div class="flex justify-center">
+        <div class="text-5xl text-white mx-20 mt-8">
+            <div class="text-center">
+                @foreach($users as $index => $user)
+                    @if($user->id === Auth::user()->id)
+                        @php
+                            $userPosition = $index + 1;
+                        @endphp
+                        <div class="bg-zinc-900 rounded hover:bg-red-400 p-2 mb-3">{{ $userPosition }}. {{ $user->name }} ({{ $user->score }})</div>
+                    @else
+                        <div class="mb-3">{{ $index+1 }}. {{ $user->name }} ({{ $user->score }})</div>
+                    @endif
+                @endforeach
+            </div>
+        </div>
     </div>
+
+
+
 </x-app-layout>
