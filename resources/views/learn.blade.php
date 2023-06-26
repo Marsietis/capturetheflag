d92bf4414d2b195a4f1c0d12bbf2371946ddc0f3
Marsietis <martynas.matijosius@gmail.com>
2023-06-26 15:07:23 +0300

    Added some info to learn.blade.php

--- resources/views/learn.blade.php
+++ resources/views/learn.blade.php
@@ -5,171 +5,29 @@
         </h2>
     </x-slot>
     <div class="container mt-20 mx-auto px-4 sm:px-6 lg:px-8 text-white">
-        <div class="text-5xl">CTF cheatsheet</div>
 
-        <div class="text-4xl">Linux Basics</div>
-
-        <h3 class="mt-4">File Operations</h3>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>View EXIF data of a file: <code>exiftool filename</code></li>
-            <li>Open a file: <code>xdg-open filename</code></li>
-            <li>Open the current folder in a file explorer: <code>xdg-open .</code></li>
-            <li>Open a file (GNOME): <code>eog filename</code></li>
-            <li>Convert hexadecimal to ASCII: <code>echo -n 0x39 | xxd -r -p</code></li>
-            <li>Write a secret message to a file: <code>echo 'Secret message.' > secret.txt</code></li>
-            <li>List files in a website using a wordlist: <code>dirb &lt;target-website&gt; /path/to/wordlist.txt</code>
-            </li>
+        <div class="text-4xl mb-2">Linux Basics</div>
+        <div class="text-2xl">File Operations</div>
+        <ul>
+            <li>View EXIF data of a file: `exiftool filename`</li>
+            <li>Open a file: `xdg-open filename`</li>
+            <li>Open the current folder in a file explorer: `xdg-open .`</li>
+            <li>Open a file (GNOME): `eog filename`</li>
+            <li>Convert hexadecimal to ASCII: `echo -n 0x39 | xxd -r -p`</li>
+            <li>Write a secret message to a file: `echo 'Secret message.' > secret.txt`</li>
+            <li>List files in a website using a wordlist: `dirb target-website /path/to/wordlist.txt`</li>
+        </ul>
+        <div class="text-2xl">Steganography</div>
+        <ul>
+            <li>Hide a message in an image: `steghide embed -ef secret.txt -cf DSCN0042.jpg`</li>
+            <li>Extract a message from an image: `steghide extract -sf DSCN0042.jpg -xf secret_extract.txt`</li>
+        </ul>
+        <div class="text-2xl">Forensics</div>
+        <ul>
+            <li>binwalk: Use binwalk to analyze the file's contents and identify embedded files, hidden data, or appended archives. Run the following command to extract any embedded files `binwalk -e filename`</li>
+            <li>Extract files with binwalk: `binwalk --dd='.*' file.pptm`</li>
+            <li>View EXIF data of a file: `exiftool filename`</li>
         </ul>
 
-        <h3 class="mt-4">Steganography</h3>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>Hide a message in an image: <code>steghide embed -ef secret.txt -cf DSCN0042.jpg</code></li>
-            <li>Extract a message from an image: <code>steghide extract -sf DSCN0042.jpg -xf secret_extract.txt</code>
-            </li>
-        </ul>
-
-        <h3 class="mt-4">Forensics</h3>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>binwalk: Use binwalk to analyze the file's contents and identify embedded files, hidden data, or
-                appended archives. Run the following command to extract any embedded files <code>binwalk -e
-                    filename</code></li>
-            <li>Extract files with binwalk: <code>binwalk --dd='.*' file.pptm</code></li>
-            <li>View EXIF data of a file: <code>exiftool filename</code></li>
-        </ul>
-
-        <h3 class="mt-4">Directory and File Operations</h3>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>Move to the previous directory: <code>cd -</code></li>
-            <li>List files and directories with detailed information: <code>ls -la</code></li>
-            <li>Display file content: <code>cat filename</code></li>
-            <li>Display readable text from a file: <code>strings filename</code></li>
-            <li>Display usage information: <code>--help</code></li>
-            <li>Create a directory: <code>mkdir</code></li>
-            <li>Create a file: <code>touch</code></li>
-            <li>Get details about a file: <code>stat filename</code></li>
-            <li>View command history with specified word: <code>history | grep mkdir</code></li>
-        </ul>
-
-        <h3 class="mt-4">File Removal (rm)</h3>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>Remove a file: <code>rm filename</code></li>
-            <li>Remove an empty directory: <code>rm -d directory</code></li>
-            <li>Remove a non-empty directory: <code>rm -rf</code></li>
-        </ul>
-
-        <h3 class="mt-4">File Copying (cp)</h3>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>Copy a file: <code>cp file.txt file_backup.txt</code></li>
-            <li>Copy a file to a directory: <code>cp file.txt /tmp</code></li>
-            <li>Copy a directory: <code>cp -R Pictures /opt/backup</code></li>
-        </ul>
-
-        <h3 class="mt-4">File Movement (mv)</h3>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>Move a file to a directory: <code>mv file.txt /tmp</code></li>
-            <li>Rename a file: <code>mv file.txt file1.txt</code></li>
-            <li>Move multiple files to a directory: <code>mv file.txt file1.txt /tmp</code></li>
-        </ul>
-
-        <h2 class="mt-8">Linux File Permissions</h2>
-
-        <p class="mt-4">
-            In Linux, each file is associated with an owner, a group, and permission access rights for three different
-            classes of users: owner (user), group members (group), and everybody else (others). The following permission
-            types apply to each class:
-        </p>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>Read permission: 'r'</li>
-            <li>Write permission: 'w'</li>
-            <li>Execute permission: 'x'</li>
-        </ul>
-
-        <p class="mt-4">
-            The permissions number of a specific user class is represented by the sum of the values of the permissions
-            for that group:
-        </p>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>'r' (read) = 4</li>
-            <li>'w' (write) = 2</li>
-            <li>'x' (execute) = 1</li>
-            <li>No permissions = 0</li>
-        </ul>
-
-        <p class="mt-4">
-            To set permissions, combine the values for each class and order them as follows:
-        </p>
-
-        <p class="mt-4"><code>&lt;owner permissions&gt;&lt;group permissions&gt;&lt;others permissions&gt;</code></p>
-
-        <p class="mt-4">
-            For example, to set read, write, and execute permissions for the owner, read and write permissions for the
-            group, and read permissions for everybody else, use the value <code>764</code>.
-        </p>
-
-        <h3 class="mt-4">Changing Permissions (chmod)</h3>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>Change permissions of a file: <code>chmod 600 new_file.txt</code></li>
-        </ul>
-
-        <h3 class="mt-4">Changing Ownership (chown)</h3>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>Change ownership of a file: <code>chown username filename.txt</code></li>
-        </ul>
-
-        <h2 class="mt-8">Finding Files (find)</h2>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>Find files with a partial name: <code>find /etc -name '*.conf'</code></li>
-            <li>Find files with an exact filename: <code>find /etc -name 'client.conf'</code></li>
-        </ul>
-
-        <p class="mt-4">
-            The asterisk '*' character can be used in the following ways:
-        </p>
-
-        <ul class="list-disc list-inside ml-4">
-            <li><code>file*</code>: Search for all files and folders that begin with 'file'</li>
-            <li><code>*file</code>: Search for all files and folders that end with 'file'</li>
-            <li><code>*file*</code>: Search for all files and folders that have 'file' in any position</li>
-        </ul>
-
-        <h2 class="mt-8">Text Searching (grep)</h2>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>Search for a word in a file: <code>grep 'Directory' /etc/rsyslog.conf</code></li>
-            <li>Case-insensitive search: <code>grep -i 'Directory' /etc/*.conf</code></li>
-            <li>Search recursively in a directory: <code>grep -i -R 'Directory' /etc/</code></li>
-        </ul>
-
-        <h2 class="mt-8">Downloading Files (wget)</h2>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>Download a specified file and change its save file name: <code>wget
-                    https://filesamples.com/samples/document/txt/sample1.txt -O new_name.txt</code></li>
-        </ul>
-
-        <h2 class="mt-8">Base64 Encoding and Decoding</h2>
-
-        <ul class="list-disc list-inside ml-4">
-            <li>Encode text to Base64: <code>echo -n 'Text that will be base64 encoded' | base64</code></li>
-            <li>Multiple rounds of Base64 encoding: <code>echo -n 'Text that will be base64 encoded' | base64 |
-                    base64</code></li>
-            <li>Decode Base64 to text: <code>echo -n 'VGV4dCB0aGF0IHdpbGwgYmUgYmFzZTY0IGVuY29kZWQ=' | base64
-                    --decode</code></li>
-            <li>Multiple rounds of Base64 decoding: <code>echo -n
-                    'VkdWNGRDQjBhR0YwSUhkcGJHd2dZbVVnWW1GelpUWTBJR1Z1WTI5a1pXUT0K' | base64 -d | base64 -d</code> (can
-                be shortened to <code>-d</code>)
-            </li>
-            <li>Decode Base64 to text with multiple rounds: <code>echo -n </li>
+    </div>
 </x-app-layout>
