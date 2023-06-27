c6e5fd42b021dc60f26f7d1caa45750868d6a899
Marsietis <martynas.matijosius@gmail.com>
2023-06-27 16:28:26 +0300

    Styling

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 min-h-screen">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center mb-6">
             <div class="bg-white dark:bg-zinc-900 overflow-hidden shadow-sm sm:rounded-lg shadow-xl">
                 <div class="p-6 text-gray-900 dark:text-gray-100 text-xl">
