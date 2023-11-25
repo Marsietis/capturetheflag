5de1a105f72fa2932e6a1ddab23a6e9bf8316631
Marsietis <martynas.matijosius@gmail.com>
2023-11-26 00:10:25 +0200

    Refactor leaderboard view and controller for enhanced usability
    Updated the leaderboard.blade.php view to display more user-relevant information and provide better visualisation of leaderboard entries. The changes include display of user's score, position and the last task solved. The leaderboard controller has been updated to fetch and process related data more efficiently using Eloquent methods. This will improve performance by eliminating unnecessary database queries and reduce code redundancy.

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Leaderboard;
 use App\Models\User;
 
 class LeaderboardController extends Controller
@@ -9,9 +10,21 @@ class LeaderboardController extends Controller
     public function __invoke()
     {
         $user = auth()->user();
-        $users = User::where('score', '>', 0)->orderByDesc('score')->get();
+
+        // get all users which have more than 0 score in leaderboard table
+        $users = User::whereHas('leaderboard', function ($query) {
+            $query->where('score', '>', 0);
+        })->with('leaderboard')->get();
+
+        // sort users by score
+        $users = $users->sortByDesc(function ($user) {
+            return $user->leaderboard->score;
+        });
+
         $userPosition = $users->pluck('id')->search($user->id) + 1;
         $userPosition = $userPosition ?: null;
-        return view('leaderboard', compact('users', 'user', 'userPosition'));
+
+        $userScore = Leaderboard::where('user_id', $user->id)->first()->score;
+        return view('leaderboard', compact('users', 'user', 'userPosition', 'userScore'));
     }
 }
