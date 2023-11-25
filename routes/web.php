a2426cd667c1a1d12f7024f3defe65b39eaffab4
Marsietis <martynas.matijosius@gmail.com>
2023-11-25 23:13:27 +0200

    Removed LearnController and simplified route definitions
    The LearnController was deleted and its functionality has been simplified and consolidated directly into the routes file. Instead of having a separate controller for returning a view, we now return the 'learn' view via a new route using Route::view. This streamlines the application and reduces unnecessary code complexity and overhead.

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,11 @@
 
 use App\Http\Controllers\LeaderboardController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\LearnController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('welcome');
 
 Route::get('/admin', function () {
     return view('admin');
@@ -24,7 +21,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('tasks', TasksController::class)->only(['index', 'store']);
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
     Route::get('/leaderboard', LeaderboardController::class)->name('leaderboard');
-    Route::get('/learn', [LearnController::class, 'index'])->name('learn');
+    Route::view('/learn', 'learn')->name('learn');
 });
 
 require __DIR__ . '/auth.php';
