830ace406fb378fb96117e77dcacf3b2c59857e1
Marsietis <martynas.matijosius@gmail.com>
2023-11-25 20:56:14 +0200

    Updated application logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,39 +1,29 @@
 <svg
     width="100"
     height="50"
-    viewBox="0 0 501.84937 217.59869"
-    id="svg5"
+    viewBox="0 0 186.48048 85.21814"
+    version="1.1"
+    id="svg1"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:svg="http://www.w3.org/2000/svg">
-    <sodipodi:namedview
-        id="namedview7"
-        pagecolor="#ffffff"
-        bordercolor="#000000"
-        borderopacity="0.25"
-        inkscape:showpageshadow="2"
-        inkscape:pageopacity="0.0"
-        inkscape:pagecheckerboard="0"
-        inkscape:deskcolor="#d1d1d1"
-        inkscape:document-units="px"
-        showgrid="false" />
     <defs
-        id="defs2" />
+        id="defs1" />
     <g
         id="layer1"
-        transform="translate(-6.3391328,-140.70282)">
+        transform="translate(-11.128693,-24.498033)">
         <text
             xml:space="preserve"
-            style="font-size:119.346px;line-height:0.8;fill:#ffffff;stroke-width:9.94549"
-            x="3.9522126"
-            y="235.1055"
-            id="text113"><tspan
-                id="tspan111"
-                x="3.9522126"
-                y="235.1055"
-                style="font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-family:Comfortaa;-inkscape-font-specification:'Comfortaa Bold';stroke-width:9.94549">Capture</tspan><tspan
-                x="3.9522126"
-                y="331.20999"
-                id="tspan117"
-                style="font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-family:Comfortaa;-inkscape-font-specification:'Comfortaa Bold';stroke-width:9.94549">the flag</tspan></text>
+            style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:46.8949px;line-height:39.8803px;font-family:'URW Gothic L';-inkscape-font-specification:'URW Gothic L';fill:#ffffff;stroke-width:3.9079"
+            x="10.808123"
+            y="59.760799"
+            id="text1"><tspan
+                style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;line-height:39.8803px;font-family:'URW Gothic L';-inkscape-font-specification:'URW Gothic L';stroke-width:3.9079"
+                id="tspan3"
+                x="10.808123"
+                y="59.760799">Capture</tspan><tspan
+                style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;line-height:39.8803px;font-family:'URW Gothic L';-inkscape-font-specification:'URW Gothic L';stroke-width:3.9079"
+                x="10.808123"
+                y="99.641098"
+                id="tspan2">The Flag</tspan></text>
     </g>
 </svg>
