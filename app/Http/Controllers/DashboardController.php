284f214881c1338414309e17ff137a4e5b474e2e
Marsietis <martynas.matijosius@gmail.com>
2023-11-26 23:46:56 +0200

    Update DashboardController and dashboard view for task filtering
    The DashboardController's __invoke method was now updated to accept a Request object to allow task filtering based on search, category, and completed status. A form was also added to the dashboard view for the new filtering options. This was done to enhance user experience, giving them the ability to easily sift through tasks.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,18 +5,29 @@ namespace App\Http\Controllers;
 use App\Models\CompletedTask;
 use App\Models\Leaderboard;
 use App\Models\Task;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         $user = auth()->user();
-        $tasks = Task::orderBy('points')->get();
+        $searchQuery = $request->input('search');
+        $categoryFilter = $request->input('category');
+        $hideCompleted = $request->input('hideCompleted');
+
+        // Retrieve tasks and apply search, category, and completed filters
+        $tasks = Task::when($searchQuery, function ($query) use ($searchQuery) {
+            return $query->where('title', 'like', '%' . $searchQuery . '%');
+        })->when($categoryFilter, function ($query) use ($categoryFilter) {
+            return $query->where('category', $categoryFilter);
+        })->when($hideCompleted, function ($query) use ($user) {
+            return $query->whereNotIn('id', CompletedTask::where('user_id', $user->id)->pluck('task_id')->toArray());
+        })->orderBy('points')->get();
+
         $completedTasks = CompletedTask::where('user_id', $user->id)->pluck('task_id')->toArray();
         $totalTaskCount = $tasks->count();
         $score = Leaderboard::where('user_id', $user->id)->value('score');
 
-        return view('dashboard', compact('tasks', 'completedTasks', 'totalTaskCount', 'score'));
-    }
-
-}
+        return view('dashboard', compact('tasks', 'completedTasks', 'totalTaskCount', 'score', 'hideCompleted'));
+    }}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -39,7 +39,7 @@
 
     <div class="py-12 min-h-screen">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center mb-6">
-            <div class="bg-white dark:bg-zinc-900 overflow-hidden shadow-sm sm:rounded-lg shadow-xl">
+            <div class="bg-white dark:bg-zinc-900 overflow-hidden sm:rounded-lg shadow-xl">
                 <div class="p-6 text-gray-900 dark:text-gray-100 text-xl">
                     Your score: {{ $score }} Tasks completed: {{ count($completedTasks) }}
                     / {{ $totalTaskCount }}                </div>
@@ -47,6 +47,40 @@
         </div>
 
         <div class="text-gray-50 text-xl mt-10">
+            <div class="mb-4">
+                <form method="GET" action="{{ route('dashboard') }}">
+                    <label for="hideCompleted" class="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                        Hide Completed Tasks
+                    </label>
+                    <input type="checkbox" id="hideCompleted" name="hideCompleted" class="mr-2" {{ $hideCompleted ? 'checked' : '' }}>
+                    <label for="search" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Search by Task Title:</label>
+                    <div class="flex mb-2">
+                        <input type="text" id="search" name="search" class="mt-1 p-2 border border-gray-300 dark:border-gray-700 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm rounded-md text-black" placeholder="Enter task title..." value="{{ request()->get('search') }}">
+                        <button type="submit" class="ml-2 inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-800 transition duration-150 ease-in-out">
+                            Search
+                        </button>
+                        <a href="{{ route('dashboard') }}" class="ml-2 inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-gray-500 hover:bg-gray-400 focus:outline-none focus:border-gray-600 focus:shadow-outline-gray active:bg-gray-600 transition duration-150 ease-in-out">
+                            Clear Filters
+                        </a>
+                    </div>
+
+                    <div>
+                        <label for="category" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Filter by Category:</label>
+                        <select id="category" name="category" class="text-black mt-1 p-2 border border-gray-300 dark:border-gray-700 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm rounded-md">
+                            <option value="" {{ request()->get('category') == '' ? 'selected' : '' }}>All Categories</option>
+                            <option value="General" {{ request()->get('category') == 'General' ? 'selected' : '' }}>General</option>
+                            <option value="Web" {{ request()->get('category') == 'Web' ? 'selected' : '' }}>Web</option>
+                            <option value="Forensics" {{ request()->get('category') == 'Forensics' ? 'selected' : '' }}>Forensics</option>
+                            <option value="Reverse engineering" {{ request()->get('category') == 'Reverse engineering' ? 'selected' : '' }}>Reverse engineering</option>
+                            <option value="Steganography" {{ request()->get('category') == 'Steganography' ? 'selected' : '' }}>Steganography</option>
+                            <option value="Cryptography" {{ request()->get('category') == 'Cryptography' ? 'selected' : '' }}>Cryptography</option>
+                        </select>
+                    </div>
+                </form>
+            </div>
+            @if ($tasks->isEmpty())
+                <p>No tasks found.</p>
+            @else
             <div
                 class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-8 md:px-16 lg:px-24">
                 @foreach($tasks as $task)
@@ -68,6 +102,7 @@
                     </div>
                 @endforeach
             </div>
+            @endif
         </div>
 
 
