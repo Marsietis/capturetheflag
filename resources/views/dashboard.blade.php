2ebb80961e3f415ac282adc02064f5b45f883e32
Marsietis <martynas.matijosius@gmail.com>
2023-11-27 12:01:21 +0200

    Refactor task search to a separate view file
    Migrated task search/filtering feature into its separate view file named search.blade.php. This makes the code for the dashboard view cleaner and more readable. Also, this allows us to reuse the search form in other views if required in the future. The existing form in dashboard.blade.php was removed and replaced with an include statement for the new search.blade.php view file.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -47,86 +47,39 @@
         </div>
 
         <div class="text-gray-50 text-xl mt-10">
-            <div class="mb-4">
-                <form method="GET" action="{{ route('dashboard') }}">
-                    <label for="hideCompleted" class="block text-sm font-medium text-gray-700 dark:text-gray-300">
-                        Hide Completed Tasks
-                    </label>
-                    <input type="checkbox" id="hideCompleted" name="hideCompleted"
-                           class="mr-2" {{ $hideCompleted ? 'checked' : '' }}>
-                    <label for="search" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Search by
-                        Task Title:</label>
-                    <div class="flex mb-2">
-                        <input type="text" id="search" name="search"
-                               class="mt-1 p-2 border border-gray-300 dark:border-gray-700 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm rounded-md text-black"
-                               placeholder="Enter task title..." value="{{ request()->get('search') }}">
-                        <button type="submit"
-                                class="ml-2 inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-800 transition duration-150 ease-in-out">
-                            Search
-                        </button>
-                        <a href="{{ route('dashboard') }}"
-                           class="ml-2 inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-gray-500 hover:bg-gray-400 focus:outline-none focus:border-gray-600 focus:shadow-outline-gray active:bg-gray-600 transition duration-150 ease-in-out">
-                            Clear Filters
-                        </a>
-                    </div>
-
-                    <div>
-                        <label for="category" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Filter
-                            by Category:</label>
-                        <select id="category" name="category"
-                                class="text-black mt-1 p-2 border border-gray-300 dark:border-gray-700 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm rounded-md">
-                            <option value="" {{ request()->get('category') == '' ? 'selected' : '' }}>All Categories
-                            </option>
-                            <option value="General" {{ request()->get('category') == 'General' ? 'selected' : '' }}>
-                                General
-                            </option>
-                            <option value="Web" {{ request()->get('category') == 'Web' ? 'selected' : '' }}>Web</option>
-                            <option value="Forensics" {{ request()->get('category') == 'Forensics' ? 'selected' : '' }}>
-                                Forensics
-                            </option>
-                            <option
-                                value="Reverse engineering" {{ request()->get('category') == 'Reverse engineering' ? 'selected' : '' }}>
-                                Reverse engineering
-                            </option>
-                            <option
-                                value="Steganography" {{ request()->get('category') == 'Steganography' ? 'selected' : '' }}>
-                                Steganography
-                            </option>
-                            <option
-                                value="Cryptography" {{ request()->get('category') == 'Cryptography' ? 'selected' : '' }}>
-                                Cryptography
-                            </option>
-                        </select>
-                    </div>
-                </form>
+            <div class="flex items-center justify-center h-full">
+                <div class="">
+                    @include('layouts.search')
+                    @if ($tasks->isEmpty())
+                        <p class="text-white text-5xl font-bold text-center mt-20">No tasks found.</p>
+                    @else
+                </div>
             </div>
-            @if ($tasks->isEmpty())
-                <p>No tasks found.</p>
-            @else
-                <div
-                    class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-8 md:px-16 lg:px-24">
-                    @foreach($tasks as $task)
-                        <div class="card w-full sm:w-full md:w-96 bg-zinc-900 shadow-xl">
-                            <div class="card-body">
-                                <div class="inline-flex items-center">
-                                    <div class="text-gray-400 mr-2 text-lg">{{ $task->category }}</div>
-                                    <div class="badge bg-zinc-800 text-white text-lg p-4">{{$task->points}} points</div>
-                                </div>
-                                <h2 class="card-title text-2xl truncate">{{$task->title}}</h2>
-                                    <div class="">{{ $task->solve_count }} solves</div>
-                                <div class="card-actions justify-end">
-                                    @if(in_array($task->id, $completedTasks))
-                                        <span class="text-gray-400 ml-2">Completed</span>
-                                    @else
-                                        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
-                                            <button class="btn btn-outline btn-error">Go to task</button>
-                                        </a>
-                                    @endif
-                                </div>
+
+            <div
+                class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-8 md:px-16 lg:px-24">
+                @foreach($tasks as $task)
+                    <div class="card w-full sm:w-full md:w-96 bg-zinc-900 shadow-xl">
+                        <div class="card-body">
+                            <div class="inline-flex items-center">
+                                <div class="text-gray-400 mr-2 text-lg">{{ $task->category }}</div>
+                                <div class="badge bg-zinc-800 text-white text-lg p-4">{{$task->points}} points</div>
+                            </div>
+                            <h2 class="card-title text-2xl truncate">{{$task->title}}</h2>
+                            <div class="">{{ $task->solve_count }} solves</div>
+                            <div class="card-actions justify-end">
+                                @if(in_array($task->id, $completedTasks))
+                                    <span class="text-gray-400 ml-2">Completed</span>
+                                @else
+                                    <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                                        <button class="btn btn-outline btn-error">Go to task</button>
+                                    </a>
+                                @endif
                             </div>
                         </div>
-                    @endforeach
-                </div>
+                    </div>
+                @endforeach
+            </div>
             @endif
         </div>
 
